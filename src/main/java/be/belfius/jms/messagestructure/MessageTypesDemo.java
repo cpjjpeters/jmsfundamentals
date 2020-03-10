79cb4655610e9ea3520a09b8ae2930bd28b4831f
cpjjpeters <cpjjpeters@gmail.com>
2020-03-10 14:49:48 +0100

    Objects used

--- src/main/java/be/belfius/jms/messagestructure/MessageTypesDemo.java
+++ src/main/java/be/belfius/jms/messagestructure/MessageTypesDemo.java
@@ -1,5 +1,7 @@
 package be.belfius.jms.messagestructure;
 
+import java.io.Serializable;
+
 import javax.jms.BytesMessage;
 import javax.jms.JMSConsumer;
 import javax.jms.JMSContext;
@@ -8,6 +10,7 @@ import javax.jms.JMSProducer;
 import javax.jms.MapMessage;
 import javax.jms.Message;
 import javax.jms.MessageProducer;
+import javax.jms.ObjectMessage;
 import javax.jms.Queue;
 import javax.jms.StreamMessage;
 import javax.jms.TextMessage;
@@ -33,6 +36,11 @@ public class MessageTypesDemo {
 			BytesMessage bytesMessage = jmsContext.createBytesMessage();
 			StreamMessage streamMessage = jmsContext.createStreamMessage();
 			MapMessage mapMessage = jmsContext.createMapMessage();
+			ObjectMessage objectMessage = jmsContext.createObjectMessage();
+			Patient patient = new Patient();
+			patient.setId(456);
+			patient.setName("Paul");
+			objectMessage.setObject(patient);
 			bytesMessage.writeUTF("John");
 			bytesMessage.writeLong(123L);
 			streamMessage.writeBoolean(true);
@@ -42,7 +50,8 @@ public class MessageTypesDemo {
 //			producer.send(queue, textMessage);
 //			producer.send(queue, bytesMessage);
 //			producer.send(queue, streamMessage);
-			producer.send(queue, mapMessage);
+//			producer.send(queue, mapMessage);
+			producer.send(queue, objectMessage);
 			
 //			BytesMessage messageReceived =  (BytesMessage) jmsContext.createConsumer(queue).receive(5000);
 //			System.out.println(""+ messageReceived.readUTF());
@@ -50,9 +59,14 @@ public class MessageTypesDemo {
 //			StreamMessage messageReceived =  (StreamMessage) jmsContext.createConsumer(queue).receive(5000);
 //			System.out.println("streamMessage: "+messageReceived.readBoolean());
 //			System.out.println(messageReceived.readFloat());
-			MapMessage messageReceived =  (MapMessage) jmsContext.createConsumer(queue).receive(5000);
-			System.out.println("mapMessage: "+messageReceived);
-			System.out.println(messageReceived.getBoolean("isCreated"));
+//			MapMessage messageReceived =  (MapMessage) jmsContext.createConsumer(queue).receive(5000);
+//			System.out.println("mapMessage: "+messageReceived);
+//			System.out.println(messageReceived.getBoolean("isCreated"));
+			ObjectMessage messageReceived =  (ObjectMessage) jmsContext.createConsumer(queue).receive(5000);
+			System.out.println("objectMessage: "+messageReceived);
+			Patient object = (Patient) messageReceived.getObject();
+			System.out.println(patient.getId());
+			System.out.println(patient.getName());
 			
 			
 		} 
